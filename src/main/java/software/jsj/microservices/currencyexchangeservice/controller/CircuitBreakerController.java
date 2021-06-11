7b4ea7014ec16ad5f51140fe64bdb3e6851b8ed8
jsjackson <jsjackson@example.com>
2021-06-12 00:39:42 +1000

    rate limiting & Bulkhead features

--- src/main/java/software/jsj/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ src/main/java/software/jsj/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package software.jsj.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +16,10 @@ public class CircuitBreakerController {
     private final Logger log = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    //@RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String sampleApi() {
         log.info("Sample API call received");
         ResponseEntity<String> responseEntity = new RestTemplate().getForEntity(
