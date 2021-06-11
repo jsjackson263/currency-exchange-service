550d6eadf73bd114f826228c95d043e5a2bcb942
jsjackson <jsjackson@example.com>
2021-06-12 00:04:14 +1000

    implement Retry & Fallback methods - resilience4j

---
+++ src/main/java/software/jsj/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,27 @@
+package software.jsj.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+    private final Logger log = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    public String sampleApi() {
+        log.info("Sample API call received");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity(
+                "http://localhost:8080/some-dummy-url", String.class);
+        return responseEntity.getBody();
+    }
+
+    private String hardcodedResponse(Throwable ex) {
+        return "fallback-response: " + ex.getMessage();
+    }
+}
